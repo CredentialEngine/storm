c44be87038e52d9c40f26d9f3d2e86fca66c74ca
Ronald Ray <fritz.ray@eduworks.com>
2018-09-05 08:50:02 -0700

    Style and smoothing out bugs.

--- dist/viewOutputFramework.js
+++ dist/viewOutputFramework.js
@@ -1,10 +1,10 @@
 $("#selectedServer").change(function (evt) {
-    if ($("#rad8:selected").length > 0)
+    if ($("#rad8:checked").length > 0)
         $("#viewOutputFramework").attr("src", "cass-editor/index.html?server=" + $("#selectedServer :selected").attr("value") + "&ceasnDataFields=true");
 });
 
 $("#rad8").change(function (evt) {
-
+    if ($("#selectedServer :selected").attr("value") == "http://nowhere/") return;
     if ($("#rad8:checked").length > 0)
         $("#viewOutputFramework").attr("src", "cass-editor/index.html?server=" + $("#selectedServer :selected").attr("value") + "&ceasnDataFields=true");
     else
